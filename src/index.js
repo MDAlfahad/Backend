032cc1ea87fa68e9408f935359d71eefbfe288d6
MDAlfahad <alfahadkhan715@gmail.com>
2026-08-14 22:24:41 +0530

    feat: initialize Express app and set up server connection

--- src/index.js
+++ src/index.js
@@ -3,4 +3,22 @@ import connectDb from "./db/database.js";
 
 configDotenv()
 
+
 connectDb()
+
+.then(()=>{
+    app.on((err)=>{
+        console.log("Server Side Error");
+        throw err
+        
+    })
+    app.listen(process.env.PORT || 4000, ()=>{
+    console.log(`Server is running on port ${process.env.PORT}`);
+    
+    })
+
+})
+.catch((err)=>{
+    console.log("MongoDb connection failed !!!", err);
+    
+})
